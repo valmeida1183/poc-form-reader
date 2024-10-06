144764725bb4eafc09233f736017a12384713080
Vinicius Lopes de Almeida <termobin@gmail.com>
2024-10-06 18:43:25 -0300

    Finished POC lib

--- projects/poc-reader-lib/src/lib/poc-reader-lib.service.ts
+++ projects/poc-reader-lib/src/lib/poc-reader-lib.service.ts
@@ -11,6 +11,7 @@ export class PocReaderLibService {
     const users = signal<UserInterface[]>([
       { id: '1', name: 'Foo' },
       { id: '2', name: 'Bar' },
+      { id: '3', name: 'Baz' },
     ]);
 
     return users;
